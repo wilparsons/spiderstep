7f34facbe0ceca91814d03945e68665c61738741
Wil Parsons <entrocraft@gmail.com>
2024-03-08 11:33:09 -0800

    Adding the test case grid.

---
+++ test.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "spiderstep.h"
+
+int main(void) {
+  unsigned long grid_height = 10;
+  unsigned long grid_width = 10;
+  unsigned long source = 0;
+  unsigned long destination = 95;
+  long step;
+  unsigned char grid[100] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  };
+  unsigned char i = 0;
+
+  grid[source] = 1;
+
+  while (source != destination) {
+    source += spiderstep(grid_width, grid_height, source, destination);
+    grid[source] = 3;
+  }
+
+  grid[destination] = 2;
+
+  while (i != 100) {
+    printf("%u ", grid[i]);
+
+    if (((i + 1) % 10) == 0) {
+      printf("\n");
+    }
+
+    i++;
+  }
+
+  return 0;
+}
