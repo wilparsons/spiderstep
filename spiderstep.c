00b2247afeab86b933ae16bce255c6898ffb523f
Wil Parsons <entrocraft@gmail.com>
2024-03-09 14:54:53 -0800

    Removing the first conditional that usually evaluates to true.

--- spiderstep.c
+++ spiderstep.c
@@ -7,33 +7,31 @@ long spiderstep(const unsigned long grid_width, const unsigned long grid_height,
   const unsigned long destination_y = destination / grid_height;
   long step = 0;
 
-  if (source != destination) {
-    if (source_x == destination_x) {
-      if (source_y < destination_y) {
-        step = grid_width;
+  if (source_x == destination_x) {
+    if (source_y < destination_y) {
+      step = grid_width;
+    } else {
+      step = -grid_width;
+    }
+  } else {
+    if (source_y == destination_y) {
+      if (source_x < destination_x) {
+        step = 1;
       } else {
-        step = -grid_width;
+        step = -1;
       }
     } else {
-      if (source_y == destination_y) {
-        if (source_x < destination_x) {
-          step = 1;
+      if (source_x < destination_x) {
+        if (source_y < destination_y) {
+          step = grid_width + 1;
         } else {
-          step = -1;
+          step = -grid_width + 1;
         }
       } else {
-        if (source_x < destination_x) {
-          if (source_y < destination_y) {
-            step = grid_width + 1;
-          } else {
-            step = -grid_width + 1;
-          }
+        if (source_y < destination_y) {
+          step = grid_width - 1;
         } else {
-          if (source_y < destination_y) {
-            step = grid_width - 1;
-          } else {
-            step = -grid_width - 1;
-          }
+          step = -grid_width - 1;
         }
       }
     }
