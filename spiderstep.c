60ce69c97f9af7613b8fbaef74bef09730bde24a
Wil Parsons <entrocraft@gmail.com>
2024-03-09 10:35:51 -0800

    Correcting width coordinate calculations.

--- spiderstep.c
+++ spiderstep.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
+#include "spiderstep.h"
 
 long spiderstep(const unsigned long grid_width, const unsigned long grid_height, const unsigned long source, const unsigned long destination) {
-  unsigned long source_x = source / grid_width;
+  const unsigned long source_x = source % grid_width;
   const unsigned long source_y = source / grid_height;
-  unsigned long destination_x = destination / grid_width;
+  const unsigned long destination_x = destination % grid_width;
   const unsigned long destination_y = destination / grid_height;
   long step = 0;
-
-  source_x = source - ((source_x << 3) + (source_x << 1));
-  destination_x = destination - ((destination_x << 3) + (destination_x << 1));
 
   if (source != destination) {
     if (source_x == destination_x) {
