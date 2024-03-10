15b6bb34304779f92b1a8849d2e7a751056411ae
Wil Parsons <entrocraft@gmail.com>
2024-03-09 16:47:14 -0800

    Decreasing CPU time and comparisons for the majority case.

--- spiderstep.c
+++ spiderstep.c
@@ -1,17 +1,19 @@
 #include "spiderstep.h"
 
 long spiderstep(const unsigned long grid_width, const unsigned long grid_height, const unsigned long source, const unsigned long destination) {
-  const unsigned long source_x = source % grid_width;
   const unsigned long source_y = source / grid_height;
-  const unsigned long destination_x = destination % grid_width;
+  const unsigned long source_x = source - (grid_width * source_y);
   const unsigned long destination_y = destination / grid_height;
-  long step = 0;
+  const unsigned long destination_x = destination - (grid_width * destination_y);
+  long step = grid_width;
 
   if (source_x == destination_x) {
-    if (source_y < destination_y) {
-      step = grid_width;
-    } else {
+    if (source_y > destination_y) {
       step = -grid_width;
+    } else {
+      if (source_y == destination_y) {
+        step = 0;
+      }
     }
   } else {
     if (source_y == destination_y) {
